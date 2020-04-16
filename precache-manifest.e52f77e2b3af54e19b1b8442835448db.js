c7fef599a718f00433066bce6c6465f28563e888
Darren Stevens <webmaster@celticwebdesign.net>
2020-04-16 22:18:55 +0100

    Updates

---
+++ precache-manifest.e52f77e2b3af54e19b1b8442835448db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8d1b3e9c4ed1113844fa56e108d3b4",
+    "url": "/create-react-app-project-1/index.html"
+  },
+  {
+    "revision": "13618837af62cfbafd02",
+    "url": "/create-react-app-project-1/static/css/2.494e9aaa.chunk.css"
+  },
+  {
+    "revision": "483cfe1df239594b598a",
+    "url": "/create-react-app-project-1/static/css/main.883c3695.chunk.css"
+  },
+  {
+    "revision": "13618837af62cfbafd02",
+    "url": "/create-react-app-project-1/static/js/2.f6a1f469.chunk.js"
+  },
+  {
+    "revision": "73e28b592dd67902e31e0a6ae5e69383",
+    "url": "/create-react-app-project-1/static/js/2.f6a1f469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "483cfe1df239594b598a",
+    "url": "/create-react-app-project-1/static/js/main.309e9b7e.chunk.js"
+  },
+  {
+    "revision": "d8622de0c69f13f63245",
+    "url": "/create-react-app-project-1/static/js/runtime-main.903ff6f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app-project-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
